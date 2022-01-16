cd40a67ab6ba2890f6fb8a62af4d938f2cf3b748
Damilare Durojaye <damilare.durojaye@nordsec.com>
2022-01-16 23:51:01 +0100

    Queues

---
+++ src/main/java/com/ceenuts/WorkingWithQueues.java
@@ -0,0 +1,23 @@
+package com.ceenuts;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class WorkingWithQueues {
+
+    public static void main(String[] args) {
+        Queue<Person> supermarket = new LinkedList<>();
+
+        supermarket.add(new Person("Alex", 21));
+        supermarket.add(new Person("Mariam", 18));
+        supermarket.add(new Person("Ali", 40));
+
+        System.out.println(supermarket.size());
+        System.out.println(supermarket.peek());
+        System.out.println(supermarket.poll());
+        System.out.println(supermarket.size());
+        System.out.println(supermarket.peek());
+    }
+
+    record Person(String name, int aga){}
+}
